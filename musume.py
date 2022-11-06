b5be00ee737b5272ce03b21de9b936cebff40240
お米美味しい <mimic0924444@gmail.com>
2022-11-07 00:38:48 +0900

    area_checkを全部の数字に対応させたぞい

--- musume.py
+++ musume.py
@@ -48,60 +48,17 @@ def swap_check():
     swap_check2(val)
 
 
-#−１はどこにあるかどうかを調べる。
-def erea_check(rows):
-    ys,xs =np.where(rows == -1)
-    x1 = xs[0]
-    x2 = xs[1]
-    y1 = ys[0]
-    y2 = ys[1]
-    #一つ目の−１の位置
-    vacant1 = [y1,x1]
-    #2つ目の−１の位置
-    vacant2 = [y2,x2]
-    return x1,y1,x2,y2
-
-#上下左右を判定し、配列外の場合、Falseを返す
-def sidecheck(rows,x,y):
-    flag1 =rows[y,x-1]
-    flag2 =rows[y,x+1]
-    flag3 =rows[y-1,x]
-    flag4 =rows[y+1,x]
-    flag5 =99
-
-
-    if x-1 >0:
-        return True, flag1,11
-    if x+1 <5:
-        return True,flag2,22
-    if y-1 >0:
-        return True,flag3,33
-    if y+1 <5:
-        return True,flag4,44
-    else:
-        print("この数字は動かせません、他の数字を入力してください")
-        return False,flag5,55
-# aはどのような場合に移動できるかを定める関数
-def sidebyside(rows,a,y1,x1,y2,x2):
-    if a == 6 or 7 or 8 or 9:
-        swap(rows, y1, x1, y2, x2)
-
-
-
-
-#上下、もしくは左右に移動可能な場合、aがvacantと隣接しているかを確認する
-def side_or_notside(rows,a,x1,y1,x2,y2):
-    point1 = sidecheck(rows,y1,x1)
-    point2 = sidecheck(rows,y2,x2)
-  
-
+#選択した数字はどこにあるかどうかを調べる。
+def area_check(rows,a):
+    A=np.where(rows == a)
+    yaxis= A[0]
+    xaxis= A[1]
+    yaxis_and_xaxis=[]
+    for y,x in zip(yaxis,xaxis):
+        yaxis_and_xaxis.append((y,x))
         
-    
-        
+    return yaxis_and_xaxis
 
 
-
-        
-
-    
-
+rows =init_puzzle()
+print(area_check(rows,0))
--- test_musume.py
+++ test_musume.py
@@ -11,20 +11,12 @@ def test_swap_check2():
     assert musume.swap_check2("9") == 9
     assert musume.swap_check2("10") is None
 
-def test_erea_check():
-    rows0 = np.array([
-        [1,-1,2],
-        [-1,3,4],
-        [5,6,7],
+def test_area_check(): 
+    a = np.array([
+        [0,0,0,0],
+        [1,1,1,1],
+        [2,2,2,3],
+        [4,5,6,7]
     ])
-    assert musume.erea_check(rows0) == (1,0,0,1)
-    
-    rows1 = np.array([
-        [0,1,-1,2,0],
-        [0,-1,3,4,0],
-        [0,5,6,7,0],
-    ])
-    assert musume.erea_check(rows1) == (2,0,1,1)
-
-
+    assert musume.area_check(a,0) == [(0,0),(0,1),(0,2),(0,3)]
 
